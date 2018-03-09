332155f21e882aac71bea07a029eee6efa865d4e
Don Smith <dons0369@gmail.com>
2018-03-09 15:54:18 -0500

    Add project files.

---
+++ TFS/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TFS
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Enter the Number");
+         int i = int.Parse(Console.ReadLine());
+
+         while (i > 0)
+         {
+            Console.WriteLine(i);
+            i = i - 2;
+         }
+      }
+   }
+}
